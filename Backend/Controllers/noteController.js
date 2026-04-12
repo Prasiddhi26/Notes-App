4ea9314e02e2fcdc949976ab9f55ea134b41d43f
Prasiddhi Shetty <shettyprasiddhi954@gmail.com>
2026-04-12 13:15:07 +0530

    Implemented get all notes API

--- Backend/Controllers/noteController.js
+++ Backend/Controllers/noteController.js
@@ -1,5 +1,20 @@
-export const getNotes = (req, res) => {
-  res.json({ message: "Get all notes" });
+import Note from "../Models/noteModel.js";
+
+// GET all notes
+export const getNotes = async (req, res) => {
+  try {
+    const notes = await Note.find(); // fetch all notes
+
+    res.status(200).json({
+      success: true,
+      data: notes,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 export const createNote = (req, res) => {
