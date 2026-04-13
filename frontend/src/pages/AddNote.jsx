a216d314fd39bdd488ac46d138ec81fa92d58ed5
Prasiddhi Shetty <shettyprasiddhi954@gmail.com>
2026-04-13 09:56:20 +0530

    Added AddNote form with validation and navigation

--- frontend/src/pages/AddNote.jsx
+++ frontend/src/pages/AddNote.jsx
@@ -1,7 +1,68 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 function AddNote() {
-    return ( 
-        <h1>AddNote page</h1>
-     );
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!title || !content) {
+      alert("Please fill all fields");
+      return;
+    }
+    console.log("Title:", title);
+    console.log("Content:", content);
+
+    //clear form after submit
+    setTitle("");
+    setContent("");
+  };
+  const navigate = useNavigate();
+  const handleAddNote = () => {
+    navigate("/");
+  };
+
+  return (
+    <div
+      className="container d-flex justify-content-center align-items-center"
+      style={{ height: "80vh" }}
+    >
+      <div className="card shadow p-4" style={{ width: "400px" }}>
+        <h3 className="text-center mb-4">Add New Note</h3>
+
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+
+          <div className="mb-3">
+            <textarea
+              className="form-control"
+              placeholder="Enter content"
+              rows="4"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            ></textarea>
+          </div>
+
+          <button
+            onClick={handleAddNote}
+            type="submit"
+            className="btn btn-success w-100"
+          >
+            Add Note
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default AddNote;
